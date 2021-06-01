b8b7a3a9e6cf3e0a1bedc7030c927f5d2480a1bd
haocongxu <haocongxu@tencent.com>
2021-06-01 20:10:28 +0800

    up

--- js/prefetch.js
+++ js/prefetch.js
@@ -1,9 +1,18 @@
+function getParam(url, key) {
+  if (!key) return null
+  var m = url.match(new RegExp('(\\?|&)' + key + '=([^&]+)'))
+  return m ? m[2].split('#')[0] : null
+}
+
 WeixinPrefetcher.config({
   shouldFetchContent(url, extInfo) {
     return true
   },
   getFetchPkgType(url, extInfo) {
     return 2
+  },
+  getId(url) {
+    return getParam(url, 'album_id')
   }
 })
 
